d24e0e3602e0856062ee0b2847796be978c7c140
Damian Wilkolek <dwilkolek@protonmail.com>
2025-12-07 18:16:31 +0100

    day 7, part 2

--- day7.c
+++ day7.c
@@ -14,64 +14,70 @@ int main(void) {
     if (stream == NULL)
         exit(EXIT_FAILURE);
 
-    const int LIMIT = 150;
+    const int LIMIT = 160;
 
-    char plan[LIMIT][LIMIT];
-    for (int i = 0; i < LIMIT; i++) {
-        for (int j = 0; j < LIMIT; j++) {
-            plan[i][j] = '.';
-        }
-    }
+    long long int plan[LIMIT][LIMIT];
+
 
     int line_no = 2;
-    int start_x = 0;
-    int start_y = 0;
+    const int start_int = 1;
+    const int dash_int = -5;
+    const int empty_int = 0;
+
+    for (int i = 0; i < LIMIT; i++) {
+        for (int j = 0; j < LIMIT; j++) {
+            plan[i][j] = empty_int;
+        }
+    }
 
     while (getline(&line, &len, stream) != -1) {
         for (int i = 0; i < strlen(line); i++) {
             if (line[i] == '\n') {
                 continue;
             }
+            int ch_int = empty_int;
             if (line[i] == 'S') {
-                start_x = i;
-                start_y = line_no;
+                ch_int = start_int;
+            } else if (line[i] == '^') {
+                ch_int = dash_int;
             }
-            plan[line_no][i + 2] = line[i];
+            plan[line_no][i + 2] = ch_int;
         }
         line_no++;
-    }
-
-    for (int i = 0; i < LIMIT; i++) {
-        for (int j = 0; j < LIMIT; j++) {
-            printf("%c", plan[i][j]);
-        }
-        printf("\n");
     }
 
 
     for (int row = 0; row < LIMIT; row++) {
         for (int i = 0; i < LIMIT - 1; i++) {
-            char ch = plan[row][i];
-            char below = plan[row + 1][i];
-            if (ch == 'S' || ch == '|') {
-                if (below == '^') {
-                    plan[row + 1][i - 1] = '|';
-                    plan[row + 1][i + 1] = '|';
+            long long int ch = plan[row][i];
+            long long int below = plan[row + 1][i];
+            if (below >= empty_int && ch >= empty_int) {
+                plan[row + 1][i] = plan[row + 1][i] + ch;
+            }
+            if (ch == start_int || ch > empty_int) {
+                if (below == dash_int) {
+                    plan[row + 1][i - 1] = plan[row + 1][i - 1] + ch;
+                    plan[row + 1][i + 1] = plan[row + 1][i + 1] + ch;
                     part1++;
-                } else if (below == '.') {
-                    plan[row + 1][i] = '|';
                 }
             }
         }
     }
     for (int i = 0; i < LIMIT; i++) {
         for (int j = 0; j < LIMIT; j++) {
-            printf("%c", plan[i][j]);
+            printf("\t%lli", plan[i][j]);
         }
         printf("\n");
     }
+    long long int part2 = 0;
+    for (int i = 0; i < LIMIT; i++) {
+        if (plan[LIMIT - 1][i] > 0) {
+            part2 += plan[LIMIT - 1][i];
+        }
+    }
 
     printf("Part 1: %d\n", part1);
+    printf("Part 2: %lli\n", part2);
 
     free(line);
     fclose(stream);
